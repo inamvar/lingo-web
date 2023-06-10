932bf36a76da7d9baf39884a252b2e71fc08d3d6
Mahsa.g <mahsageramy.pr@gmail.com>
2023-06-10 16:32:32 +0330

    refactor: footer

--- common/constants.js
+++ common/constants.js
@@ -1,12 +1,12 @@
 export const Constants = {
     token:'accessToken',
     refreshToken:'refreshToken',
-    InstagramAddress:'آدرس صفحه اینستاگرام',
+    InstagramAddress:'InstagramAddress',
     WhatsAppAddress:'شماره واتس اپ',
-    TwitterAddress:"آدرس صفحه توییتر",
-    TelegramAddress:"آدرس کانال تلگرام",
-    YouTubeChannelAddress:"آدرس کانال یوتوب",
-    MobileAppIOSDownloadLink:"آدرس دانلود اپ IOS در سیب اپ",
-    MobileAppAndroidDownloadLink:"آدرس اپ اندروید در گوگل پلی",
-    MobileAppBazarDownloadLink:"آدرس اپ اندروید در کافه بازار"
+    TwitterAddress:"TwitterAddress",
+    TelegramAddress:"TelegramChannelAddress",
+    YouTubeChannelAddress:"YouTubeChannelAddress",
+    MobileAppIOSDownloadLink:"MobileAppIOSLink",
+    MobileAppAndroidDownloadLink:"MobileAndroidAppGooglePlayLink",
+    MobileAppBazarDownloadLink:"MobileAppAndroidBazarLink"
 };
