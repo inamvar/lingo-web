47950478214c7c8edc785dd9eb7b9ce6ac6bd0b9
Mahsa.g <mahsageramy.pr@gmail.com>
2023-09-13 13:57:35 +0330

    add: mailto for email and time for videos

--- components/accordion.jsx
+++ components/accordion.jsx
@@ -14,6 +14,7 @@ export default function accordion(props)
 {
     const chapters = props.chapters;
     const slug = props.slug;
+    console.log(chapters[0].videos);
 
     const gotoDetail=()=>
     {
@@ -56,14 +57,15 @@ export default function accordion(props)
                                         </div>
                                     </div>
 
-                                        <Link onClick={gotoDetail} href={vid.isActive?appRoutes.Video(vid.slug):appRoutes.PaymentDetail(slug)} className='flex items-center justify-center gap-1 w-1/6 sm:w-1/5'>
-                                            {vid.isActive? <p className='text-sm hidden lg:block'>نمایش</p>:<p className='text-white text-sm hidden lg:block'>نمایش</p>}
-                                            <div>
-                                                {vid.isActive?<svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1049 7.96587L2.7474 0.243722C1.6621 -0.383401 0 0.225169 0 1.77628V17.2169C0 18.6084 1.54446 19.447 2.7474 18.7494L16.1049 11.031C17.2965 10.3445 17.3003 8.65237 16.1049 7.96587Z" fill="#525252"/></svg>
-                                                    : <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.5357 11.375H19.3036V7.71875C19.3036 3.46328 15.8022 0 11.5 0C7.19777 0 3.69643 3.46328 3.69643 7.71875V11.375H2.46429C1.10379 11.375 0 12.4668 0 13.8125V23.5625C0 24.9082 1.10379 26 2.46429 26H20.5357C21.8962 26 23 24.9082 23 23.5625C23 22.2168 23 13.8125 23 13.8125C23 12.4668 21.8962 11.375 20.5357 11.375ZM15.1964 11.375H7.80357V7.71875C7.80357 5.70273 9.46183 4.0625 11.5 4.0625C13.5382 4.0625 15.1964 5.70273 15.1964 7.71875V11.375Z" fill="#9E9E9E"/></svg>
-                                                }
-                                            </div>
-                                        </Link>
+                                    <lable>{vid.duration}</lable>
+                                    <Link onClick={gotoDetail} href={vid.isActive?appRoutes.Video(vid.slug):appRoutes.PaymentDetail(slug)} className='flex items-center justify-center gap-1 w-1/6 sm:w-1/5'>
+                                        {vid.isActive? <p className='text-sm hidden lg:block'>نمایش</p>:<p className='text-white text-sm hidden lg:block'>نمایش</p>}
+                                        <div>
+                                            {vid.isActive?<svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1049 7.96587L2.7474 0.243722C1.6621 -0.383401 0 0.225169 0 1.77628V17.2169C0 18.6084 1.54446 19.447 2.7474 18.7494L16.1049 11.031C17.2965 10.3445 17.3003 8.65237 16.1049 7.96587Z" fill="#525252"/></svg>
+                                                : <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.5357 11.375H19.3036V7.71875C19.3036 3.46328 15.8022 0 11.5 0C7.19777 0 3.69643 3.46328 3.69643 7.71875V11.375H2.46429C1.10379 11.375 0 12.4668 0 13.8125V23.5625C0 24.9082 1.10379 26 2.46429 26H20.5357C21.8962 26 23 24.9082 23 23.5625C23 22.2168 23 13.8125 23 13.8125C23 12.4668 21.8962 11.375 20.5357 11.375ZM15.1964 11.375H7.80357V7.71875C7.80357 5.70273 9.46183 4.0625 11.5 4.0625C13.5382 4.0625 15.1964 5.70273 15.1964 7.71875V11.375Z" fill="#9E9E9E"/></svg>
+                                            }
+                                        </div>
+                                    </Link>
                                 </div>
                             </AccordionPanel>
                         )}
